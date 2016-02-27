029e484b4d93750e41133ab7d5c8ba427ff567dd
Julien <julien@localhost.localdomain>
2016-02-27 18:33:47 +0100

    base du métronome

--- app/js/app.js
+++ app/js/app.js
@@ -4,8 +4,40 @@
 
   angular.module('metronome', [])
 
-  .controller('main', function($scope) {
+  .controller('main', function($scope, $timeout) {
     $scope.title = 'Metronome';
+    $scope.son = 'audio/son.ogg';
+    $scope.tempo = 4; //nombre de pulsation par seconde
+    $scope.playing = false;
+
+    //Bonne pratique de pré déclaration des fonctions.
+    $scope.startStop = startStop;
+
+    var player = document.getElementById('player');
+
+    player.stop = function() {
+      player.pause();
+      player.currentTime = 0;
+    };
+
+
+    function startStop() {
+      $scope.playing = !$scope.playing;
+      if ($scope.playing) {
+        playTempo();
+      }
+    }
+
+    function playTempo() {
+      $timeout(function() {
+        player.stop();
+        player.play();
+        if ($scope.playing) {
+          playTempo();
+        }
+      }, Math.round(1 / $scope.tempo * 1000));
+    }
+
   });
 
 })();
