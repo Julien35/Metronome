308b09f6b8be7ab22bd74b3ed97dd755b4f442b3
Julien <Julien@>
2016-05-10 17:18:11 +0200

    add ui-view

--- app/js/routes.js
+++ app/js/routes.js
@@ -2,20 +2,63 @@
   'use strick';
 
   angular
-    .module('metronome')
+    .module('metronome', ['ui.router'])
     .config(config);
 
-  function config($routeProvider,
-    $locationProvider) {
+  function config($stateProvider, $urlRouterProvider) {
 
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/home.html',
-        // delete ng-controller="homeController as homeCtrl" in home.html if use
-        controller: 'homeController',
-        controllerAs: 'homeCtrl'
-      });
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider.state('home', {
+      url: '/',
+      views: {
+
+        'header': {
+          templateUrl: 'views/assets/header.html'
+        },
+
+        'nav': {
+          templateUrl: 'views/assets/nav.html'
+        },
+
+        'home': {
+          templateUrl: 'views/home.html',
+          controller: 'homeController',
+          controllerAs: 'homeCtrl'
+        },
+
+        'footer': {
+          templateUrl: 'views/assets/footer.html'
+        }
+
+      }
+    });
+
+
 
   }
 
 })();
+
+// Old version
+// (function() {
+//   'use strick';
+//
+//   angular
+//     .module('metronome')
+//     .config(config);
+//
+//   function config($routeProvider,
+//     $locationProvider) {
+//
+//     $routeProvider
+//       .when('/', {
+//         templateUrl: 'views/home.html',
+//         // delete ng-controller="homeController as homeCtrl" in home.html if use
+//         controller: 'homeController',
+//         controllerAs: 'homeCtrl'
+//       });
+//
+//   }
+//
+// })();
